284d8858e96c32b8bb1d39fdcc0c62e550f41797
Youssef Sbai <youssef.sbai@camao.one>
2022-03-23 10:10:55 +0100

    email check service was added

---
+++ Hotel.Core/EmailCheckService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Core
+{
+    public class EmailCheckService
+    {
+        public bool CheckIfEmailValid()
+        {
+            return true;
+        }
+    }
+}
