a5ebfec54ebcba710e8792287cc08493b367a79b
Youssef Sbai <youssef.sbai@camao.one>
2022-05-18 15:29:23 +0200

    email validation is now being used in the ui in the add new reservation

---
+++ Hotel.UI.Wpf/MVVM/ValidationRules/EmailValidationRule.cs
@@ -0,0 +1,35 @@
+﻿using Hotel.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Hotel.UI.Wpf.MVVM.ValidationRules
+{
+    public class EmailValidationRule : ValidationRule
+    {
+        private readonly EmailCheckService _emailCheckSerice = new EmailCheckService();
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string)
+            {
+                var isEmail = _emailCheckSerice.CheckIfEmailValid((string)value);
+                if (!isEmail)
+                {
+                    return new ValidationResult(false, "Invalid Email!");
+                }
+                else
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            else
+            {
+                return new ValidationResult(false,"Email Required!");
+            }
+        }
+    }
+}
